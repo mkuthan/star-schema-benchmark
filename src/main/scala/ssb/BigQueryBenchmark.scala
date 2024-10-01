6890e2ab515068dc84b9dbb3b2affc0b4ccaf608
Marcin Kuthan <marcin.kuthan@allegro.pl>
2024-10-01 17:17:04 +0200

    deduplicate benchmark calls

--- src/main/scala/ssb/BigQueryBenchmark.scala
+++ src/main/scala/ssb/BigQueryBenchmark.scala
@@ -1,6 +1,5 @@
 package ssb
 
-import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.BenchmarkMode
 import org.openjdk.jmh.annotations.Fork
 import org.openjdk.jmh.annotations.Measurement
@@ -21,46 +20,6 @@ object BigQueryBenchmark {
 @Warmup(iterations = 0)
 @Measurement(iterations = 5)
 @Fork(1)
-class BigQueryBenchmark {
-
-  import BigQueryBenchmark._
-
-  @Benchmark
-  def b1_1(): String = query("1_1")
-
-  @Benchmark
-  def b1_2(): String = query("1_2")
-
-  @Benchmark
-  def b1_3(): String = query("1_3")
-
-  @Benchmark
-  def b2_1(): String = query("2_1")
-
-  @Benchmark
-  def b2_2(): String = query("2_2")
-
-  @Benchmark
-  def b2_3(): String = query("2_3")
-
-  @Benchmark
-  def b3_1(): String = query("3_1")
-
-  @Benchmark
-  def b3_2(): String = query("3_2")
-
-  @Benchmark
-  def b3_3(): String = query("3_3")
-
-  @Benchmark
-  def b3_4(): String = query("3_4")
-
-  @Benchmark
-  def b4_1(): String = query("4_1")
-
-  @Benchmark
-  def b4_2(): String = query("4_2")
-
-  @Benchmark
-  def b4_3(): String = query("4_3")
+class BigQueryBenchmark extends AbstractBenchmark {
+  protected def query(scenarioName: String): String = BigQueryBenchmark.query(scenarioName)
 }

cdde5b8e2dbd2b37c72856f2edf71a96e2565824
Marcin Kuthan <marcin.kuthan@allegro.pl>
2024-10-14 10:32:13 +0200

    Introduce count distinct benchmarks for Druid

--- src/main/scala/ssb/DruidBenchmark.scala
+++ src/main/scala/ssb/DruidBenchmark.scala
@@ -2,6 +2,12 @@ package ssb
 
 import scala.annotation.unused
 
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Fork
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.Warmup
 import ssb.infrastructure.DruidClient
 import ssb.infrastructure.FileUtils
 
@@ -23,3 +29,21 @@ class DruidSerialBenchmark extends SerialBenchmark with DruidBenchmark
 
 @unused
 class DruidConcurrentBenchmark extends ConcurrentBenchmark with DruidBenchmark
+
+@BenchmarkMode(Array(Mode.SingleShotTime))
+@Warmup(iterations = 0)
+@Measurement(iterations = 1)
+@Fork(1)
+class DruidCountDistinctBenchmark {
+  @Benchmark
+  def b3_1_count_distinct(): String = BigQueryBenchmark.query("3_1_count_distinct")
+
+  @Benchmark
+  def b3_1_count_distinct_approx(): String = BigQueryBenchmark.query("3_1_count_distinct_approx")
+
+  @Benchmark
+  def b3_4_count_distinct(): String = BigQueryBenchmark.query("3_4_count_distinct")
+
+  @Benchmark
+  def b3_4_count_distinct_approx(): String = BigQueryBenchmark.query("3_4_count_distinct_approx")
+}
